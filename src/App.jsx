3cebbb5108012ac5cf18807856092ab85aba9726
Joshie-Borrow <joshuabarotea06@gmail.com>
2026-05-12 07:56:49 +0800

    Implement routing with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
-import FooterBar from "./components/layouts/FooterBar";
+// import CartPage from "./pages/CartPage";
+// import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <>
-      <FooterBar />
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        <Route element={<Layout />}>
+          <Route path="/"         element={<HomePage />} />
+          <Route path="/about"    element={<AboutPage />} />
+          <Route path="/contact"  element={<ContactPage />} />       
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
